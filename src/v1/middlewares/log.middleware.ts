c78690d7c759c6c0df31e445fe3111c596b028a3
kwn <kwn1125dev@gmail.com>
2023-05-03 22:08:16 +0900

    feat: Add error logger

--- src/v1/middlewares/log.middleware.ts
+++ src/v1/middlewares/log.middleware.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { requestLogger } from "../loggers/request.logger";
+import { errorLogger } from "../loggers/errorLogger";
 
 export const logRequest = (req: Request, _: Response, next: NextFunction) => {
   req.headers["X-Request-Id"] = uuidv4();
@@ -12,4 +13,13 @@ export const logRequest = (req: Request, _: Response, next: NextFunction) => {
     body: req.body,
   });
   next();
+};
+
+export const logError = (req: Request, status: number, errors: string[], stack?: string) => {
+  errorLogger.error({
+    requestId: req.headers["X-Request-Id"],
+    status,
+    errors,
+    stack,
+  });
 };
